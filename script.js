7e7c152cdaa44e70e2609fba05445af8baa97262
Max Sanchez <145044997+MaxSanchez99@users.noreply.github.com>
2025-09-13 18:02:39 -0500

    Fixed grid layout and added hover effects
    Made the width of the grid not span the whole
    window size to make it look better. Adding
    randomization to the color shown when mouse hovers
    over a square and added a progressive darkening
    effect.

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 function createGrid(side) {
-    let container = document.querySelector('#container');
 
-    totalArea = side * side;
+    let totalArea = side * side;
+
     for (let i = 0; i < totalArea; i++) {
         let div = document.createElement('div');
         div.style.border = '2px solid black';
-        div.style.height = '50px';
-        div.style['flex-basis'] = `calc(100% / ${side})`;
+        div.style.flexBasis = `calc(100% / ${side})`;
+        div.style.aspectRatio = '1 / 1'; // makes height match width
+        div.style.opacity = 1;
         container.appendChild(div);
     }
-
-    container.addEventListener('mouseover', (event) => {
-        if (event.target != container) {
-            event.target.style.backgroundColor = 'red';
-        }
-    })
-
-    container.addEventListener('mouseout', (event) => {
-        if (event.target != container) {
-            event.target.style.backgroundColor = '';
-        }
-    })
 }
 
-createGrid(16);
+function changeGridSize() {
+    let sideLength = -1;
+
+    while (sideLength < 0 || sideLength > 100) {
+        sideLength = Number(prompt('Enter a number for the amount of squares per side (limit 100): ', 0));
+    }
+
+    container.innerHTML = '';
+    createGrid(Number(sideLength));
+}
+
+
+let container = document.querySelector('#container');
+
+container.addEventListener('mouseover', (event) => {
+
+    if (event.target != container) {
+        let curOpacity = parseFloat(event.target.style.opacity);
+        let newOpacity = Math.max(curOpacity - 0.1, 0); // decrease
+        event.target.style.opacity = newOpacity;
+
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
+})
+
+container.addEventListener('mouseout', (event) => {
+    if (event.target != container) {
+        event.target.style.backgroundColor = '';
+    }
+})
+
+
+let button = document.querySelector('#btn');
+button.addEventListener('click', changeGridSize);
